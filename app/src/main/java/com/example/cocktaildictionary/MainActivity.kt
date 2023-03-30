8ad406fcdf9c8018b6abc41299b0e473ee0376fb
MPENDULO5 <MpenduloN@discovery.co.za>
2023-03-30 08:10:26 +0200

    fixed compile and target Sdk versions

---
+++ app/src/main/java/com/example/cocktaildictionary/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.cocktaildictionary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.cocktaildictionary.databinding.ActivityMainBinding
+import com.example.task1.network.CocktailApiServices
+import com.example.task1.network.RetrofitClientInstance
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var myCompositeDisposable: CompositeDisposable = CompositeDisposable()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        getPopularCocktails()
+    }
+
+
+    private fun getPopularCocktails() {
+        val service =
+            RetrofitClientInstance.retrofitInstance?.create(CocktailApiServices::class.java)
+
+        myCompositeDisposable.add(service!!.getCocktails()
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
+            .subscribe(
+                { res ->
+                    binding.getData.visibility = View.INVISIBLE
+                    binding.progressBar.visibility = View.INVISIBLE
+                    binding.Id.visibility = View.VISIBLE
+                    binding.Id.text = "Success: Found " + res.Cocktails.size.toString() + " Cocktails"
+                },
+                { throwable ->
+                    binding.getData.visibility = View.INVISIBLE
+                    binding.progressBar.visibility = View.INVISIBLE
+                    binding.Id.visibility = View.VISIBLE
+                    binding.Id.text = "Failure: "+throwable.message.toString()
+                }
+            )
+        )
+
+    }
+
+
+}
